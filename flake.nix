20ec628e7565ddbe7c6e970312bd499de5561ddf
Giovanni d'Amelio <gio@damelio.net>
2023-03-23 18:33:16 +0000

    Very simple generated flake following unstable with home manager

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Simple System Config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
