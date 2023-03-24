5c1bf3a6872dbd541bcd85a7e474ed72fead3f5e
Giovanni d'Amelio <gio@damelio.net>
2023-03-24 06:11:57 +0000

    Fish: set fish as the default shell

--- common/users/giodamelio.nix
+++ common/users/giodamelio.nix
@@ -6,6 +6,7 @@
 
   users.users.giodamelio = {
     isNormalUser = true;
+    shell = pkgs.fish;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbUQ/gUuzZbOtBPseVWL1GVrjP08JqdNwHdndQgH+Am giodamelio@penguin"
