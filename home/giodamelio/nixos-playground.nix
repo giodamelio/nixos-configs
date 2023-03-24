47b6eb05519c32a29344c3c33b90033caa2485db
Giovanni d'Amelio <gio@damelio.net>
2023-03-24 00:28:23 +0000

    Configure the git email and username

--- home/giodamelio/nixos-playground.nix
+++ home/giodamelio/nixos-playground.nix
@@ -3,6 +3,8 @@
     home-manager.enable = true;
     git = {
       enable = true;
+      userName = "Giovanni d'Amelio";
+      userEmail = "gio@damelio.net";
       aliases = {
         ci = "commit";
         st = "status";
