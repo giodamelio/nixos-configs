4be932e66a0e746f0be15fcab454ce3f0876d945
Giovanni d'Amelio <gio@damelio.net>
2023-03-24 22:58:08 +0000

    Neovim: Add gitgutter and gitlinker

--- home/giodamelio/features/neovim/config/lua/plugins.lua
+++ home/giodamelio/features/neovim/config/lua/plugins.lua
@@ -177,8 +177,54 @@ return require('lazy').setup({
   },
 
   -- Lists make your troubles go away!
+  { 'folke/trouble.nvim' },
+
+  -- Show git status in gutter
   {
-    'folke/trouble.nvim'
+    'lewis6991/gitsigns.nvim',
+    dependencies = {
+      { 'ruifm/gitlinker.nvim' }
+    },
+    config = function()
+      local gs = require('gitsigns')
+      local gsa = require('gitsigns.actions')
+      local gl = require('gitlinker')
+      local wk = require('which-key')
+
+      gs.setup({
+        current_line_blame = true,
+        keymaps = {}, -- Remove default bindings since we add our own
+      })
+
+      gl.setup({
+        mappings = nil
+      })
+
+      -- Setup some keybindings
+      wk.register({
+        g = {
+          name = 'Git',
+          n = { function() gsa.next_hunk() end, 'Go to next hunk' },
+          p = { function() gsa.prev_hunk() end, 'Go to previous hunk' },
+          s = { function() gs.stage_hunk() end, 'Stage hunk' },
+          u = { function() gs.undo_stage_hunk() end, 'Unstage hunk' },
+          r = { function() gs.reset_hunk() end, 'Reset hunk' },
+          b = { function() gs.blame_line(true) end, 'Blame Current Line' }, -- true shows full blame with a diff
+          y = { function() gl.get_buf_range_url('n') end, 'Get Permalink' },
+        },
+      }, { prefix = '<leader>' })
+
+      -- Some visual mode keybindings
+      -- TODO: these seem to be broken
+      wk.register({
+        g = {
+          name = 'Git',
+          s = { function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, 'Stage hunk' },
+          r = { function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, 'Reset hunk' },
+          y = { function() gl.get_buf_range_url('v') end, 'Get Permalink' },
+        },
+      }, { prefix = '<leader>', mode = 'v' })
+    end
   },
 
   -- Language support
