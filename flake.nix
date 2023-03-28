2d834544c6d0cd8da264ab95169be211518fd8bb
Giovanni d'Amelio <gio@damelio.net>
2023-03-28 11:25:02 -0700

    Typo

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
       nodes = {
         cadmium = {
           hostname = "localhost";
-          ssh_user = "root";
+          sshUser = "root";
           profiles.system = {
             user = "root";
             path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations."cadmium";
