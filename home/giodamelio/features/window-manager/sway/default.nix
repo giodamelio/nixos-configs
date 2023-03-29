d455429a4d0e896ac8ab76a9dccbfb95436088d0
Giovanni d'Amelio <gio@damelio.net>
2023-03-29 10:58:08 -0700

    Sway: change mod and set default terminal

--- home/giodamelio/features/window-manager/sway/default.nix
+++ home/giodamelio/features/window-manager/sway/default.nix
@@ -4,5 +4,13 @@
 
   wayland.windowManager.sway = {
     enable = true;
+    config = {
+      modifier = "Mod4";
+      terminal = "${pkgs.kitty}/bin/kitty";
+    };
   };
+
+  home.packages = with pkgs; [
+    kitty
+  ];
 }
