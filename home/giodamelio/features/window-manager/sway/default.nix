da138d3b9b1b5a217edae4a7591f9857c0f536ce
Giovanni d'Amelio <gio@damelio.net>
2023-03-29 12:35:51 -0700

    Sway: setup monitor positions

--- home/giodamelio/features/window-manager/sway/default.nix
+++ home/giodamelio/features/window-manager/sway/default.nix
@@ -7,10 +7,35 @@
     config = {
       modifier = "Mod4";
       terminal = "${pkgs.kitty}/bin/kitty";
+
+      # Configure monitors
+      # Remember to take scaling into account when calculating positions
+      # The height of 420 is to vertically align the landscape monitors with the portrait one
+      #
+      # +------+
+      # |      | +----------+ +----------+
+      # |      | |          | |          |
+      # | DP-2 | |   DP-3   | |   DP-1   |
+      # |      | |          | |          |
+      # |      | +----------+ +----------+
+      # +------+
+      output = {
+        DP-2 = {
+          transform = "270";
+          scale = "2";
+          position = "0 0";
+        };
+
+        DP-3 = {
+          scale = "2";
+          position = "1080 420";
+        };
+
+        DP-1 = {
+          scale = "2";
+          position = "3000 420";
+        };
+      };
     };
   };
-
-  home.packages = with pkgs; [
-    kitty
-  ];
 }
