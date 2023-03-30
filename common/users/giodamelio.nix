23b85c11067dd8a342d45e4e983a1a820be712c4
Giovanni d'Amelio <gio@damelio.net>
2023-03-30 16:59:04 -0700

    Nixos: make fish my user shell again via a hack

--- common/users/giodamelio.nix
+++ common/users/giodamelio.nix
@@ -4,10 +4,13 @@
   # TODO: should probabaly enable this
   # users.mutableUsers = false;
 
+  # TODO: this should be in home manager somehow...
+  programs.fish.enable = true;
+
   users.users.giodamelio = {
     isNormalUser = true;
     # TODO: should make this work again
-    # shell = pkgs.fish;
+    shell = pkgs.fish;
     extraGroups = [ "wheel" "video" "audio" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbUQ/gUuzZbOtBPseVWL1GVrjP08JqdNwHdndQgH+Am giodamelio@penguin"
