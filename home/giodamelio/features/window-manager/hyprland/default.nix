30abe051325330b1a3c3250b4c8c154b320363ab
Giovanni d'Amelio <gio@damelio.net>
2023-03-30 14:12:36 -0700

    Waybar: simple waybar config

--- home/giodamelio/features/window-manager/hyprland/default.nix
+++ home/giodamelio/features/window-manager/hyprland/default.nix
@@ -15,4 +15,48 @@
   # home.packages = [
   #   inputs.hyprland.nixosModules.default
   # ];
+  programs.waybar = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.waybar-hyprland;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        output = [
+          "DP-3"
+        ];
+
+        modules-left = [ "wlr/workspaces" ];
+        modules-center = [ "clock" ];
+
+        clock = {
+          format = "{:%I:%M%p}";
+          format-alt = "{:%A, %B %d, %Y (%R)}";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+        };
+
+        "wlr/workspaces" = {
+          format = "{icon}";
+        };
+      };
+      leftBar = {
+        layer = "top";
+        position = "top";
+        output = [
+          "DP-2"
+        ];
+
+        modules-left = [ "wlr/workspaces" ];
+      };
+      rightBar = {
+        layer = "top";
+        position = "top";
+        output = [
+          "DP-1"
+        ];
+
+        modules-left = [ "wlr/workspaces" ];
+      };
+    };
+  };
 }
