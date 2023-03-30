7a41d4209f872fea37d1999d572fa83faf4b67cd
Giovanni d'Amelio <gio@damelio.net>
2023-03-29 23:01:49 -0700

    Add some more groups to my user

--- common/users/giodamelio.nix
+++ common/users/giodamelio.nix
@@ -7,7 +7,7 @@
   users.users.giodamelio = {
     isNormalUser = true;
     shell = pkgs.fish;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "video" "audio" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbUQ/gUuzZbOtBPseVWL1GVrjP08JqdNwHdndQgH+Am giodamelio@penguin"
     ];
