cb333a19d96a459b75df1fe99c4e42a8a00159ad
Giovanni d'Amelio <gio@damelio.net>
2023-03-30 12:02:44 -0700

    Sway: apperently enabling opengl makes sway work

--- hosts/cadmium/default.nix
+++ hosts/cadmium/default.nix
@@ -46,6 +46,10 @@
   # Enable polkit
   security.polkit.enable = true;
 
+  # Enable OpenGL
+  # This was needed to make Sway work
+  hardware.opengl.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
