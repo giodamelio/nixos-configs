ca6d619b28b3a549f0a34b474b19a83cdd617bc7
Giovanni d'Amelio <gio@damelio.net>
2023-04-18 12:34:48 -0700

    Create a fresh flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "My NixOS Systems";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
