e4aae4fd99c2f53aa42ddb8069172418a3f6e03d
giodamelio <giodamelio@mynixos.com>
2023-07-25 21:55:38 +0000

    Release #4

--- nixosConfigurations/beryllium.nix
+++ nixosConfigurations/beryllium.nix
@@ -8,6 +8,30 @@ let
           enable = true;
         };
       };
+      security = {
+        sudo = {
+          wheelNeedsPassword = false;
+        };
+      };
+      users = {
+        users = {
+          server = {
+            extraGroups = [
+              "wheel"
+            ];
+            isNormalUser = true;
+            openssh = {
+              authorizedKeys = {
+                keys = [
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKaKx5hPY3/SHy+xezxd5IsCmDoFTMIbqxTonmVVC0GB giodamelio@DESKTOP-LP2IMU5"
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAZF+j6HGldFqQdp+CaPaYKGMsFpUsk49jqhb7VtdUvn giodamelio@cadmium"
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbUQ/gUuzZbOtBPseVWL1GVrjP08JqdNwHdndQgH+Am giodamelio@penguin"
+                ];
+              };
+            };
+          };
+        };
+      };
     };
   };
 in
