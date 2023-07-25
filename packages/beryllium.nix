72bffdbeb58edeacb4e7c7147778fba15cf5c052
giodamelio <giodamelio@mynixos.com>
2023-07-25 23:47:32 +0000

    Release #5

--- packages/beryllium.nix
+++ packages/beryllium.nix
@@ -1,6 +1,44 @@
 { inputs, ... }@flakeContext:
 let
-  nixosModule = { config, lib, pkgs, ... }: { };
+  nixosModule = { config, lib, pkgs, ... }: {
+    config = {
+      programs = {
+        neovim = {
+          defaultEditor = true;
+          enable = true;
+        };
+      };
+      security = {
+        sudo = {
+          wheelNeedsPassword = false;
+        };
+      };
+      services = {
+        openssh = {
+          enable = true;
+        };
+      };
+      users = {
+        users = {
+          server = {
+            extraGroups = [
+              "wheel"
+            ];
+            isNormalUser = true;
+            openssh = {
+              authorizedKeys = {
+                keys = [
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKaKx5hPY3/SHy+xezxd5IsCmDoFTMIbqxTonmVVC0GB giodamelio@DESKTOP-LP2IMU5"
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAZF+j6HGldFqQdp+CaPaYKGMsFpUsk49jqhb7VtdUvn giodamelio@cadmium"
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbUQ/gUuzZbOtBPseVWL1GVrjP08JqdNwHdndQgH+Am giodamelio@penguin"
+                ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 in
 inputs.nixos-generators.nixosGenerate {
   system = "x86_64-linux";
