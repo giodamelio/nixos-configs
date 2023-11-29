3107b51115d71f83ac906a86f30f8aba8686d4bc
Giovanni d'Amelio <gio@damelio.net>
2023-11-29 01:18:18 +0000

    Add disk format and barebones config for cadmium

---
+++ src/disko/systems/cadmium.nix
@@ -0,0 +1,80 @@
+{root, ...}: {
+  imports = [
+    # Setup the bootloader to handle zfs
+    root.nixosModules.core-bootloader-zfs
+  ];
+
+  config = {
+    disko.devices = {
+      disk = {
+        a = {
+          type = "disk";
+          device = "/dev/disk/by-id/scsi-SNVMe_INTEL_SSDPEKNW010000_0000_0100_0000_E4D2_5C33_2A5C_5101.";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              };
+              zfs = {
+                size = "100%";
+                content = {
+                  type = "zfs";
+                  pool = "tank";
+                };
+              };
+            };
+          };
+        };
+      };
+      zpool = {
+        tank = {
+          type = "zpool";
+          postCreateHook = "zfs snapshot tank@blank";
+          rootFsOptions = {
+            compression = "zstd";
+            mountpoint = "none";
+          };
+
+          datasets = {
+            reserve = {
+              type = "zfs_fs";
+              options = {
+                mountpoint = "none";
+                reservation = "5G";
+              };
+            };
+            root = {
+              type = "zfs_fs";
+              mountpoint = "/";
+              options = {
+                mountpoint = "legacy";
+              };
+              postCreateHook = "zfs snapshot tank/root@blank";
+            };
+            nix = {
+              type = "zfs_fs";
+              mountpoint = "/nix";
+              options = {
+                mountpoint = "legacy";
+              };
+            };
+            home = {
+              type = "zfs_fs";
+              mountpoint = "/home";
+              options = {
+                mountpoint = "legacy";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
---
+++ src/nixosConfigurations/cadmium.nix
@@ -0,0 +1,20 @@
+{
+  root,
+  inputs,
+  ...
+}:
+inputs.nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+
+  modules = [
+    # Disk layout
+    inputs.disko.nixosModules.disko
+    root.disko.systems.cadmium
+
+    # Boot with grub
+    root.nixosModules.core-bootloader-grub
+
+    # Basic packages I want on every system
+    root.nixosModules.basic-packages
+  ];
+}
