1e53a5dde4ab0c8e124ba5bc11f50ae61c0ffcd1
Giovanni d'Amelio <gio@damelio.net>
2023-12-21 16:48:55 -0600

    Formatting

--- src/packages/generate-readme.nix
+++ src/packages/generate-readme.nix
@@ -5,23 +5,24 @@
 }: {pkgs}: let
   lib = pkgs.lib;
   machineListItems = lib.attrsets.mapAttrsToList (name: value: "${name}=${value}");
-in pkgs.stdenv.mkDerivation {
-  name = "generate-readme";
+in
+  pkgs.stdenv.mkDerivation {
+    name = "generate-readme";
 
-  src = lib.fileset.toSource {
-    root = ../..;
-    fileset = lib.fileset.union ./README.md.tmpl ../../homelab.toml;
-  };
-  unpackPhase = "true";
+    src = lib.fileset.toSource {
+      root = ../..;
+      fileset = lib.fileset.union ./README.md.tmpl ../../homelab.toml;
+    };
+    unpackPhase = "true";
 
-  buildInputs = [ pkgs.gomplate ];
+    buildInputs = [pkgs.gomplate];
 
-  buildPhase = ''
-    gomplate --file $src/src/packages/README.md.tmpl --out README.md --datasource homelab=file://$src/homelab.toml
-  '';
+    buildPhase = ''
+      gomplate --file $src/src/packages/README.md.tmpl --out README.md --datasource homelab=file://$src/homelab.toml
+    '';
 
-  installPhase = ''
-    mkdir $out/
-    cp README.md $out/
-  '';
-}
+    installPhase = ''
+      mkdir $out/
+      cp README.md $out/
+    '';
+  }
