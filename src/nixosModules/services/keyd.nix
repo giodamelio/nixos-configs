9cbb8ff74878bd8261a5dff0f0cd632274d876d9
Giovanni d'Amelio <gio@damelio.net>
2024-02-21 09:26:53 -0800

    setup keyd to make the capslock key be control and escape

---
+++ src/nixosModules/services/keyd.nix
@@ -0,0 +1,23 @@
+_: {
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  services.keyd = {
+    enable = true;
+
+    keyboards.default = {
+      ids = ["*"];
+      settings = {
+        main = {
+          # Make capslock be control when held and escape when tapped
+          capslock = "overload(control, esc)";
+
+          # Make esc be capslock
+          esc = "capslock";
+        };
+      };
+    };
+  };
+}
