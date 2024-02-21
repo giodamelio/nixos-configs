cae719282fb288ae60fdc05776c08c2d079804fa
Giovanni d'Amelio <gio@damelio.net>
2024-02-21 13:57:13 -0800

    Add giodamelio to docker user group

--- src/nixosModules/users/giodamelio.nix
+++ src/nixosModules/users/giodamelio.nix
@@ -1,6 +1,6 @@
 {homelab, ...}: {pkgs, ...}: {
   users.users.giodamelio = {
-    extraGroups = ["wheel"];
+    extraGroups = ["wheel" "docker"];
     isNormalUser = true;
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = homelab.ssh_keys;
