5b9a7f3523041c40bc2100d486014d16eddffb3e
Giovanni d'Amelio <gio@damelio.net>
2024-03-01 11:43:36 -0800

    Generate local cert for Kanidm and let Caddy automatically get certs

--- src/nixosModules/services/kanidm.nix
+++ src/nixosModules/services/kanidm.nix
@@ -1,49 +1,60 @@
-_: {config, ...}: {
-  security.lego = {
-    enable = true;
-    acceptTerms = true;
-    email = "gio@damelio.net";
-
-    certs."testing2.gio.ninja" = {};
-    certs."testing3.gio.ninja" = {};
-    certs."testing4.gio.ninja" = {};
-  };
-
-  services.acme-redirect = {
-    enable = true;
-    acceptTerms = true;
-    email = "gio@damelio.net";
-
-    certs."testing21.gio.ninja" = {};
-    certs."testing22.gio.ninja" = {};
-    certs."testing23.gio.ninja" = {};
-  };
-
-  # Get HTTPS certificates from LetsEncrypt for Kanidm
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "gio@damelio.net";
-
-    certs."login.gio.ninja" = {
-      listenHTTP = ":8080";
+_: {pkgs, ...}: {
+  # TLS Setup script
+  # Generates key and encrypts with systemd-creds
+  # See: https://systemd.io/CREDENTIALS/
+  # To run: `sudo systemctl start kanidm-generate-tls-cert`
+  systemd.services.kanidm-generate-tls-cert = {
+    description = "Generate a TLS cert for Kanidm";
+    wantedBy = ["default.target"];
+    before = ["kanidm.service"];
+    serviceConfig = {
+      Type = "oneshot";
     };
+    unitConfig = {
+      # Only generate if an existing key doesn't exist
+      # Note negation of the path
+      ConditionPathExists = [
+        "!/usr/lib/credstore.encrypted/kanidm.crt"
+        "!/usr/lib/credstore.encrypted/kanidm.key"
+      ];
+    };
+    script = ''
+      # Ensure the location to store the cert exists
+      mkdir -p /usr/lib/credstore.encrypted/
+
+      # Move to a temporary directory
+      newdir=$(mktemp -d)
+      cd $newdir
+
+      # Generate a TLS key that will last 10 years
+      ${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
+        -nodes -keyout kanidm.key -out kanidm.crt -subj "/CN=localhost" \
+        -addext "subjectAltName=DNS:localhost,IP:127.0.0.1"
+
+      # Encrypt the keys as SystemD credentials
+      systemd-creds encrypt kanidm.crt /usr/lib/credstore.encrypted/kanidm.crt
+      systemd-creds encrypt kanidm.key /usr/lib/credstore.encrypted/kanidm.key
+
+      rm -r "$newdir"
+    '';
   };
 
-  # Load the LetsEncrypt certs as SystemD credentials
+  # Load the TLS cert from the SystemD credentials
   systemd.services.kanidm = {
-    serviceConfig = let
-      cert_dir = config.security.acme.certs."login.gio.ninja".directory;
-    in {
-      LoadCredential = [
-        "certs:${cert_dir}"
+    serviceConfig = {
+      ImportCredential = [
+        "kanidm.crt"
+        "kanidm.key"
       ];
+    };
+    environment = {
+      KANIDM_TLS_CHAIN = "%d/kanidm.crt";
+      KANIDM_TLS_KEY = "%d/kanidm.key";
     };
   };
 
   # Start Kanidm
-  services.kanidm = let
-    credentials_directory = "/run/credentials/kanidm.service";
-  in {
+  services.kanidm = {
     enableServer = true;
     serverSettings = {
       bindaddress = "0.0.0.0:8443";
@@ -51,12 +62,9 @@ _: {config, ...}: {
       origin = "https://login.gio.ninja";
       domain = "login.gio.ninja";
 
-      # Certificates from the security.acme module
-      # TODO: these should really be referencing the $CREDENTIALS_DIRECTORY not hardcoded
-      # kanidm will need some way to load config from env vars or files first
-      # See: https://github.com/kanidm/kanidm/issues/290
-      tls_key = "${credentials_directory}/certs_key.pem";
-      tls_chain = "${credentials_directory}/certs_fullchain.pem";
+      # This are placeholders, since Kanidm will use the ENV vars before the config
+      tls_key = "/dev/null";
+      tls_chain = "/dev/null";
 
       online_backup = {
         path = "/var/lib/kanidm/backups/";
@@ -71,25 +79,34 @@ _: {config, ...}: {
     };
   };
 
+  # Load the TLS cert from the SystemD credentials
+  systemd.services.caddy = {
+    serviceConfig = {
+      ImportCredential = [
+        "kanidm.crt"
+      ];
+    };
+    environment = {
+      KANIDM_TLS_CHAIN = "%d/kanidm.crt";
+    };
+  };
+
   # Use Caddy to reverse proxy
   services.caddy = {
     enable = true;
-    group = "acme";
-
     virtualHosts."https://login.gio.ninja" = {
-      useACMEHost = "login.gio.ninja";
       extraConfig = ''
         reverse_proxy https://localhost:8443 {
           transport http {
+            # Not sure why this doesn't work...
+            # Maybe related to how Go checks SANs?
+            # See: https://github.com/docker/for-linux/issues/248
+            # tls_trusted_ca_certs {$KANIDM_TLS_CHAIN}
+
+            # I don't like this, see above
             tls_insecure_skip_verify
           }
         }
-      '';
-    };
-
-    virtualHosts."http://login.gio.ninja" = {
-      extraConfig = ''
-        reverse_proxy http://localhost:8080
       '';
     };
   };
