e72e01c55a9f951f502f56c2acd2a843946ff971
Giovanni d'Amelio <gio@damelio.net>
2024-03-03 16:36:23 -0800

    Rekey to add another user public key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   giodamelio-cadmium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOKmSxFyT9n91A9dOpSCfl9kJj80KWFA6UvCtguT4S5b giodamelio@cadmium";
-  users = [giodamelio-cadmium];
+  giodamelio-cesium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHlH3cxSO186g1bcZ3I3xSX3Fi2E094XnzvTFnW5/G1 giodamelio@cesium";
+  users = [giodamelio-cadmium giodamelio-cesium];
 
   zirconium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIECR2kS4hXBLqvqK21Ko+4CborL0Uo64/ZvtrISCsKPS";
   systems = [zirconium];
