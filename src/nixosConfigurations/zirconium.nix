876550f4fef524392ecf3d86acd18c4ab72d841a
Giovanni d'Amelio <gio@damelio.net>
2024-03-04 09:50:37 -0800

    Serve records for the nebula hosts with coredns

--- src/nixosConfigurations/zirconium.nix
+++ src/nixosConfigurations/zirconium.nix
@@ -38,11 +38,36 @@ inputs.nixpkgs.lib.nixosSystem {
     # Add Nebula mesh network
     root.nixosModules.services.nebula
 
-    (_: {
+    ({pkgs, ...}: {
       networking.hostId = "54544019";
 
       # Load the deployment config from our homelab.toml
       inherit (homelab.machines.zirconium) deployment;
+
+      services.coredns = let
+        membersList =
+          pkgs.lib.mapAttrsToList
+          (host: ip: "${ip}\t${host}.n.gio.ninja")
+          homelab.networks.nebula-homelab.members;
+        hostsFile = pkgs.writeTextFile {
+          name = "coredns-nebula-hosts";
+          text = pkgs.lib.concatStringsSep "\n" membersList;
+        };
+      in {
+        enable = true;
+        config = ''
+          n.gio.ninja {
+            hosts ${hostsFile} {
+              fallthrough
+            }
+            forward . 8.8.8.8
+          }
+
+          . {
+            forward . 8.8.8.8
+          }
+        '';
+      };
     })
   ];
 }
