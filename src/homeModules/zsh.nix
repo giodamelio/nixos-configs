81e7cf67574b5b6f35a3e8374723982ad5265ca9
Giovanni d'Amelio <gio@damelio.net>
2024-03-21 12:38:37 -0700

    Update default up arrow behavior with Atuin

--- src/homeModules/zsh.nix
+++ src/homeModules/zsh.nix
@@ -7,9 +7,9 @@ _: _: {
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
-    flags = [
-      "--disable-up-arrow"
-    ];
+    settings = {
+      filter_mode_shell_up_key_binding = "session";
+    };
   };
 
   programs.zoxide = {
