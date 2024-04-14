c39bda2a231eb61574ec531abd201141e653f901
Giovanni d'Amelio <gio@damelio.net>
2024-04-14 16:07:22 -0700

    Remove TSDB and Telegraf

--- src/nixosModules/machines/zirconium/monitoring.nix
+++ src/nixosModules/machines/zirconium/monitoring.nix
@@ -1,11 +1,4 @@
-_: {
-  pkgs,
-  config,
-  ...
-}: let
-  db_name = "tsdb";
-  user_name = db_name;
-in {
+_: {config, ...}: {
   # Setup Prometheus
   services.prometheus = {
     enable = true;
@@ -36,86 +29,6 @@ in {
         ];
       }
     ];
-  };
-
-  # Setup TimescaleDB database
-  gio.services.postgres = {
-    enable = true;
-    timescaledb = true;
-    databases = [db_name];
-  };
-  # Map telegraf system user to correct username
-  services.postgresql = {
-    authentication = ''
-      # Allow unix socket connections with a username map for tsdb
-      local ${db_name} ${user_name} peer map=tsdb
-
-      # Allow local access to only tsdb without a password
-      host ${db_name} grafana samehost trust
-    '';
-    identMap = ''
-      tsdb telegraf ${user_name}
-    '';
-  };
-
-  # Configure Telegraf to send stats to to TSDB
-  services.telegraf = {
-    enable = true;
-    extraConfig = {
-      inputs = {
-        # System Stats
-        cpu = {};
-        disk = {};
-        diskio = {};
-        kernel = {};
-        linux_sysctl_fs = {};
-        mem = {};
-        net = {
-          # Setting this to false is deprecated
-          # See: https://github.com/influxdata/telegraf/blob/master/plugins/inputs/net/README.md
-          ignore_protocol_stats = true;
-        };
-        netstat = {};
-        nstat = {};
-        processes = {};
-        smart = {
-          path_smartctl = "${pkgs.smartmontools}/bin/smartctl";
-          path_nvme = "${pkgs.nvme-cli}/bin/nvme";
-        };
-        swap = {};
-        system = {};
-        systemd_units = [
-          {unittype = "service";}
-          {unittype = "timer";}
-        ];
-        zfs = {};
-
-        # Monitor PostgreSQL
-        postgresql = {
-          address = "host=/run/postgresql user=${db_name} sslmode=disable";
-        };
-
-        # Monitor Wireguard
-        wireguard = {};
-      };
-      outputs.postgresql = {
-        connection = "host=/run/postgresql dbname=${db_name} user=${db_name} sslmode=disable";
-
-        # Make it work with TSDB
-        tags_as_foreign_keys = true;
-        create_templates = [
-          "CREATE EXTENSION IF NOT EXISTS timescaledb"
-          "CREATE TABLE {{ .table }} ({{ .columns }})"
-          "SELECT create_hypertable({{ .table|quoteLiteral }}, by_range('time', INTERVAL '1 week'), if_not_exists := true)"
-        ];
-      };
-    };
-  };
-
-  # Give Telegraf CAP_NET_ADMIN so it can talk to Wireguard via netlink
-  systemd.services.telegraf.serviceConfig = {
-    CapabilityBoundingSet = "CAP_NET_ADMIN";
-    AmbientCapabilities = "CAP_NET_ADMIN";
   };
 
   # Load the OAuth id/secret for Grafana
