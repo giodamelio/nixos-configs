231196ce8a163e3c4eae4cbc9b90fdb1b683325e
Giovanni d'Amelio <gio@damelio.net>
2024-04-23 13:23:04 -0700

    Get the StepCA server running on zirconium

--- src/nixosModules/machines/zirconium/step-ca.nix
+++ src/nixosModules/machines/zirconium/step-ca.nix
@@ -17,4 +17,22 @@ _: {pkgs, ...}: {
 
   # Setup data path so the CLI knows where to find stuff
   environment.variables.STEPPATH = "/var/lib/step/";
+
+  # Run the CA server
+  systemd.services.step-ca = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      User = "step";
+      Group = "step";
+      LoadCredentialEncrypted = "stepca-intermediate-password";
+      ExecStart = "${pkgs.step-ca}/bin/step-ca /var/lib/step/config/ca.json --password-file \${CREDENTIALS_DIRECTORY}/stepca-intermediate-password";
+    };
+  };
+
+  networking.firewall.interfaces."wg0" = {
+    allowedTCPPorts = [7443];
+  };
+  networking.firewall.interfaces."wg9" = {
+    allowedTCPPorts = [7443];
+  };
 }
