da1522c42171146fe9c7174fbaea4b5616dbba6c
Giovanni d'Amelio <gio@damelio.net>
2024-04-24 15:19:00 -0700

    Make all the servers send stats to TSDB with Telegraf

--- src/nixosModules/core/monitoring.nix
+++ src/nixosModules/core/monitoring.nix
@@ -19,6 +19,77 @@ _: {
     };
   };
 
+  # Configure Telegraf to send stats to to TSDB
+  services.telegraf = {
+    enable = true;
+    extraConfig = {
+      inputs = {
+        # System Stats
+        cpu = {};
+        disk = {};
+        diskio = {};
+        kernel = {};
+        linux_sysctl_fs = {};
+        mem = {};
+        net = {
+          # Setting this to false is deprecated
+          # See: https://github.com/influxdata/telegraf/blob/master/plugins/inputs/net/README.md
+          ignore_protocol_stats = true;
+        };
+        netstat = {};
+        nstat = {};
+        processes = {};
+        # TODO: fix this
+        # smart = {
+        #   path_smartctl = "${pkgs.smartmontools}/bin/smartctl";
+        #   path_nvme = "${pkgs.nvme-cli}/bin/nvme";
+        # };
+        swap = {};
+        system = {};
+        systemd_units = [
+          {unittype = "service";}
+          {unittype = "timer";}
+        ];
+        zfs = {
+          poolMetrics = true;
+          datasetMetrics = true;
+        };
+
+        # TODO: Fix this, what are the minimum permissions it needs to function
+        # Monitor PostgreSQL
+        # postgresql = {
+        #   address = "host=/run/postgresql user=telegraf sslmode=disable";
+        # };
+
+        # Monitor Wireguard
+        wireguard = {};
+      };
+      outputs.postgresql = {
+        connection = "host=zirconium.gio.ninja user=telegraf dbname=metrics sslmode=disable";
+
+        # Make it work with TSDB
+        tags_as_foreign_keys = true;
+        create_templates = [
+          "CREATE TABLE {{ .table }} ({{ .columns }})"
+          "SELECT create_hypertable({{ .table|quoteLiteral }}, by_range('time', INTERVAL '1 week'), if_not_exists := true)"
+        ];
+      };
+    };
+  };
+
+  systemd.services.telegraf = {
+    # Give Telegraf CAP_NET_ADMIN so it can talk to Wireguard via netlink
+    serviceConfig = {
+      CapabilityBoundingSet = "CAP_NET_ADMIN";
+      AmbientCapabilities = "CAP_NET_ADMIN";
+      LoadCredentialEncrypted = "telegraf-postgres-passfile";
+    };
+
+    environment = {
+      PGPASSFILE = "%d/telegraf-postgres-passfile";
+    };
+  };
+
   networking.firewall.interfaces."wg9" = {
     allowedTCPPorts = [
       config.services.prometheus.exporters.node.port
