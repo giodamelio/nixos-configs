1735f7b0dae6c12d770509e8875aa7bec1f74447
Giovanni d'Amelio <gio@damelio.net>
2024-06-26 21:53:18 -0700

    Update Trouble+Telescope config

--- src/packages/_neovim_lua/plugins.lua
+++ src/packages/_neovim_lua/plugins.lua
@@ -3,14 +3,14 @@ require('trouble').setup()
 
 -- Telescope
 local telescope = require('telescope')
-local trouble = require('trouble.providers.telescope')
+local trouble = require('trouble.sources.telescope')
 
 -- Allow opening telescope results in Trouble
 telescope.setup({
   defaults = {
     mappings = {
-      i = { ['<c-t>'] = trouble.open_with_trouble },
-      n = { ['<c-t>'] = trouble.open_with_trouble },
+      i = { ['<c-t>'] = trouble.open },
+      n = { ['<c-t>'] = trouble.open },
     },
   },
 })
