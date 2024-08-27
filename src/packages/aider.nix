9097947f0ba14ec90dfa5073940d8efc5b9b28d1
Giovanni d'Amelio <gio@damelio.net>
2024-08-27 11:49:19 -0700

    Update aider package to be copied and modified version from Nixpkgs

--- src/packages/aider.nix
+++ src/packages/aider.nix
@@ -1,44 +1,105 @@
-{inputs, ...}: {pkgs, ...}:
-inputs.dream2nix.lib.evalModules {
-  packageSets.nixpkgs = pkgs;
-  modules = [
-    inputs.dream2nix.modules.dream2nix.pip
-    (
-      _: rec {
-        deps = {nixpkgs, ...}: {
-          python = nixpkgs.python311;
-        };
+_: {pkgs, ...}: let
+  inherit (pkgs) lib;
+  python3 = pkgs.python311.override {
+    self = python3;
+    packageOverrides = _: super: {
+      tree-sitter = super.tree-sitter_0_21;
+      fastapi = super.fastapi.overridePythonAttrs (_: rec {
+        # Flaky test:
+        #     ResourceWarning: Unclosed <MemoryObjectSendStream>
+        # Unclear whether it's flaky in general or only in this overridden package set.
+        doCheck = false;
+      });
+    };
+  };
+  version = "0.51.0";
+in
+  python3.pkgs.buildPythonApplication {
+    pname = "aider-chat";
+    inherit version;
+    pyproject = true;
 
-        name = "aider";
-        version = "0.41.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "paul-gauthier";
+      repo = "aider";
+      rev = "refs/tags/v${version}";
+      hash = "sha256-vomRXWL3++1R8jpjMKbsGrB+B1FWQxVbLKxuPttnspw=";
+    };
 
-        mkDerivation = {
-          src = pkgs.fetchFromGitHub {
-            owner = "paul-gauthier";
-            repo = "aider";
-            rev = "v${version}";
-            hash = "sha256-vC3HiIR+5iYWsXI1pMyEmsLb5iTaHgoDPItrux7e2Vk=";
-          };
-        };
+    build-system = with python3.pkgs; [setuptools];
 
-        buildPythonPackage = {
-          pythonImportsCheck = [
-            "aider"
-          ];
-        };
+    dependencies = with python3.pkgs;
+      [
+        aiohappyeyeballs
+        backoff
+        beautifulsoup4
+        configargparse
+        diff-match-patch
+        diskcache
+        flake8
+        gitpython
+        grep-ast
+        importlib-resources
+        jsonschema
+        jiter
+        litellm
+        networkx
+        numpy
+        packaging
+        pathspec
+        pillow
+        playwright
+        prompt-toolkit
+        pypager
+        pypandoc
+        pyperclip
+        pyyaml
+        rich
+        scipy
+        sounddevice
+        soundfile
+        streamlit
+        tokenizers
+        watchdog
+      ]
+      ++ lib.optionals (!tensorflow.meta.broken) [
+        llama-index-core
+        llama-index-embeddings-huggingface
+      ];
 
-        pip = {
-          pypiSnapshotDate = "2024-05-15";
-          flattenDependencies = true;
-          requirementsFiles = [
-            "requirements.txt"
-          ];
-        };
+    buildInputs = [pkgs.portaudio];
 
-        paths.lockFile = "aider-lock.json";
-        paths.projectRoot = ./.;
-        paths.package = ./.;
-      }
-    )
-  ];
-}
+    pythonRelaxDeps = true;
+
+    nativeCheckInputs = (with python3.pkgs; [pytestCheckHook]) ++ [pkgs.gitMinimal];
+
+    disabledTestPaths = [
+      # requires network
+      "tests/scrape/test_scrape.py"
+
+      # Expected 'mock' to have been called once
+      "tests/help/test_help.py"
+    ];
+
+    disabledTests =
+      [
+        # requires network
+        "test_urls"
+        "test_get_commit_message_with_custom_prompt"
+
+        # FileNotFoundError
+        "test_get_commit_message"
+
+        # Expected 'launch_gui' to have been called once
+        "test_browser_flag_imports_streamlit"
+      ]
+      ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+        # fails on darwin
+        "test_dark_mode_sets_code_theme"
+        "test_default_env_file_sets_automatic_variable"
+      ];
+
+    preCheck = ''
+      export HOME=$(mktemp -d)
+    '';
+  }
