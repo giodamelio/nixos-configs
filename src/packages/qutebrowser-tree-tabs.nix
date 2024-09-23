6fde407514050ca6ba389292f8051fb9abffae46
Giovanni d'Amelio <gio@damelio.net>
2024-09-23 14:56:34 -0700

    Clean up unused variable

--- src/packages/qutebrowser-tree-tabs.nix
+++ src/packages/qutebrowser-tree-tabs.nix
@@ -1,22 +1,20 @@
-_: {pkgs, ...}: let
-  lib = pkgs.lib;
-in
-  pkgs.qutebrowser.overrideAttrs (_: prev: {
-    # Override source to be from the `tree-tabs-integration` branch
-    src = pkgs.fetchFromGitHub {
-      owner = "qutebrowser";
-      repo = "qutebrowser";
-      rev = "tree-tabs-integration";
-      hash = "sha256-d4Lyjj78GxdwT8bf1AgGwgzxuhMbbPiZolMWN8/V7cs=";
-    };
+_: {pkgs, ...}:
+pkgs.qutebrowser.overrideAttrs (_: prev: {
+  # Override source to be from the `tree-tabs-integration` branch
+  src = pkgs.fetchFromGitHub {
+    owner = "qutebrowser";
+    repo = "qutebrowser";
+    rev = "tree-tabs-integration";
+    hash = "sha256-d4Lyjj78GxdwT8bf1AgGwgzxuhMbbPiZolMWN8/V7cs=";
+  };
 
-    # Add some env vars to Qutebrowser to make the rendering clear with Wayland + fractional scaling
-    preFixup =
-      prev.preFixup
-      + ''
-        makeWrapperArgs+=(
-          --set QT_QPA_PLATFORM wayland
-          --set QT_SCALE_FACTOR_ROUNDING_POLICY RoundPreferFloor
-        )
-      '';
-  })
+  # Add some env vars to Qutebrowser to make the rendering clear with Wayland + fractional scaling
+  preFixup =
+    prev.preFixup
+    + ''
+      makeWrapperArgs+=(
+        --set QT_QPA_PLATFORM wayland
+        --set QT_SCALE_FACTOR_ROUNDING_POLICY RoundPreferFloor
+      )
+    '';
+})
