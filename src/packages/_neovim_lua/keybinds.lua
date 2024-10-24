a7aea882f42265fe1e079007d53eaf9069ac072a
Giovanni d'Amelio <gio@damelio.net>
2024-10-24 16:34:04 -0700

    Add keybindings for attaching and running last for neotest

--- src/packages/_neovim_lua/keybinds.lua
+++ src/packages/_neovim_lua/keybinds.lua
@@ -40,6 +40,8 @@ wk.add({
   { '<leader>ts', function() neotest.summary.toggle() end, desc = 'Toggle summary' },
   { '<leader>tt', function() neotest.run.run() end, desc = 'Run nearest test' },
   { '<leader>tw', function() neotest.watch.toggle(vim.fn.expand('%')) end, desc = 'Watch tests in file' },
+  { '<leader>ta', function() neotest.run.attach() end, desc = 'Attach to running test' },
+  { '<leader>tl', function() neotest.run.run_last() end, desc = 'Run last test' },
 })
 
 -- Language Server
