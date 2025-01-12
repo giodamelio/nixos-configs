f04656d5735680fa5fdab7333bf172ec48a40d8c
Giovanni d'Amelio <gio@damelio.net>
2025-01-12 13:46:55 -0800

    Copy over neovim package

---
+++ nix/packages/neovim/default.nix
@@ -0,0 +1,144 @@
+{ pkgs, ... }: let
+  gitlinker = pkgs.vimUtils.buildVimPlugin {
+    pname = "gitlinker.nvim";
+    version = "2023-12-15";
+    src = pkgs.fetchFromGitHub {
+      owner = "linrongbin16";
+      repo = "gitlinker.nvim";
+      rev = "542f51784f20107ef9ecdadc47825204837efed5"; # Latest on branch master as of 2024-06-26
+      hash = "sha256-OnlJf31dTzLOJ1tlDKH7slPnQGMZUloavEAtd/FxK0U=";
+    };
+    meta.homepage = "https://github.com/linrongbin16/gitlinker.nvim";
+  };
+
+  stayCentered = pkgs.vimUtils.buildVimPlugin {
+    pname = "stay-centered.nvim";
+    version = "2023-12-15";
+    src = pkgs.fetchFromGitHub {
+      owner = "arnamak";
+      repo = "stay-centered.nvim";
+      rev = "91113bd82ac34f25c53d53e7c1545cb5c022ade8"; # Latest on branch main as of 2024-06-26
+      hash = "sha256-DDhF/a8S7Z1aR1Hg8UVgttl3je0hhn/OpZoakOeMHQw=";
+    };
+    meta.homepage = "https://github.com/arnamak/stay-centered.nvim";
+  };
+
+  # parrot = let
+  #   version = "0.6.0";
+  # in
+  #   pkgs.vimUtils.buildVimPlugin {
+  #     pname = "parrot.nvim";
+  #     inherit version;
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "frankroeder";
+  #       repo = "parrot.nvim";
+  #       rev = "v${version}";
+  #       hash = "sha256-jZFnC1GA4xBRqdcSTzWGu0C0McFKro3To/gxgZayTZU=";
+  #     };
+  #     meta.homepage = "https://github.com/frankroeder/parrot.nvim";
+  #   };
+
+  nvimConfig = pkgs.neovimUtils.makeNeovimConfig {
+    withPython3 = true;
+    withRuby = false;
+    vimAlias = true;
+    viAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      # Colorscheme
+      {
+        plugin = tokyonight-nvim;
+        config = "colorscheme tokyonight";
+      }
+
+      # Interactivly show keybinds
+      which-key-nvim
+
+      # Pretty lists of things
+      trouble-nvim
+
+      # Fuzzy find things
+      telescope-nvim
+      plenary-nvim # Needed dependency
+      fzf-lua # Needed dependency
+
+      # Language Server
+      nvim-lspconfig
+
+      # Autocomplete
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      cmp-cmdline
+
+      # Snippets
+      luasnip
+      cmp_luasnip
+      friendly-snippets
+
+      # Code Context
+      nvim-navic
+
+      # AST for hightlighting, formatting, etc
+      nvim-treesitter.withAllGrammars
+      rainbow-delimiters-nvim # Rainbow parens
+      nvim-treesitter-parsers.hurl
+
+      # Status bar
+      lualine-nvim
+      lualine-lsp-progress
+      nvim-web-devicons
+
+      # Git stuff
+      gitsigns-nvim # Git status in the gutter
+      gitlinker # Easily link to specific file locations
+      neogit # Git ui
+      diffview-nvim # Better diffs
+
+      # Test running
+      neotest
+      nvim-nio # Needed dependency
+      neotest-rust
+      neotest-elixir
+      neotest-go
+      neotest-deno
+      neotest-rspec
+
+      # ChatGPT-nvim # ChatGPT integration
+      ChatGPT-nvim # ChatGPT integration
+      # parrot # GenAI Integration
+      nui-nvim # needed for ChatGPT
+      oil-nvim # Move/Create/Delete files/directories directly in a vim buffer
+      nvim-surround # Deal with pairs of things
+      comment-nvim # Better comments
+      marks-nvim # Show marks in the sign column
+      vim-eunuch # Do a Unix to it
+      bufdelete-nvim # Better behaved :Bedelete (keeps splits etc...)
+      stayCentered # Keep the cursor line centered vertically as much as possible
+      vim-startuptime # Keep on top of Neovim startup time
+      elixir-tools-nvim # Elixir tooling
+      other-nvim # Easily switch to related file types
+      nvim-notify # Pretty notifications
+    ];
+
+    customRC = "
+      luafile ${./lua/basic.lua}
+      luafile ${./lua/lsp.lua}
+      luafile ${./lua/treesitter.lua}
+      luafile ${./lua/plugins.lua}
+      luafile ${./lua/keybinds.lua}
+    ";
+  };
+in
+  pkgs.symlinkJoin {
+    name = "nvim";
+    paths = [
+      # Custom Neovim
+      (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped nvimConfig)
+
+      # Some random dependencies
+      pkgs.fzf # Fuzzy finder needed from fzf-lua plugin
+      pkgs.skim # Fuzzy finder needed from fzf-lua plugin
+    ];
+  }
---
+++ nix/packages/neovim/lua/lsp.lua
@@ -0,0 +1,181 @@
+-- Helper function for cmp/LuaSnip bindings
+local has_words_before = function()
+  -- luacheck: ignore
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local lspconfig = require('lspconfig')
+local navic = require('nvim-navic')
+
+-- Setup completion
+cmp.setup({
+  snippet = {
+    expand = function(args) require('luasnip').lsp_expand(args.body) end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end),
+  }),
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  sources = cmp.config.sources({
+    { name = 'path' },
+    { name = 'nvim_lsp', keyword_length = 2 },
+    { name = 'luasnip', keyword_length = 3 },
+  }, {
+    { name = 'buffer', keyword_length = 4 },
+  }),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' },
+  }, {
+    { name = 'cmdline' },
+  }),
+})
+
+-- Setup Language Servers
+
+-- Set the default capabilities
+local lsp_defaults = lspconfig.util.default_config
+lsp_defaults.capabilities =
+  vim.tbl_deep_extend('force', lsp_defaults.capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+-- Attach navic
+lsp_defaults.on_attach = function(client, bufnr)
+  if client.server_capabilities.documentSymbolProvider then navic.attach(client, bufnr) end
+end
+
+-- Autoformat before save
+vim.api.nvim_create_augroup('AutoFormatting', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  group = 'AutoFormatting',
+  callback = function() vim.lsp.buf.format() end,
+})
+
+-- Setup some language servers
+-- Config stolen from lsp-zero.nvim, makes Lua work good with Neovim
+-- See: https://github.com/VonHeikemen/lsp-zero.nvim/blob/dev-v3/lua/lsp-zero/server.lua#L203-L233
+local lua_ls_config = function()
+  local runtime_path = vim.split(package.path, ';')
+  table.insert(runtime_path, 'lua/?.lua')
+  table.insert(runtime_path, 'lua/?/init.lua')
+
+  return {
+    settings = {
+      Lua = {
+        -- Disable telemetry
+        telemetry = { enable = false },
+        runtime = {
+          -- Tell the language server which version of Lua you're using
+          -- (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          path = runtime_path,
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { 'vim' },
+        },
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            -- Make the server aware of Neovim runtime files
+            vim.fn.expand('$VIMRUNTIME/lua'),
+            vim.fn.stdpath('config') .. '/lua',
+          },
+        },
+      },
+    },
+  }
+end
+lspconfig.lua_ls.setup(lua_ls_config()) -- Lua
+lspconfig.nil_ls.setup({
+  settings = {
+    ['nil'] = {
+      formatting = {
+        command = { 'alejandra' },
+      },
+    },
+  },
+})
+lspconfig.gopls.setup({})
+lspconfig.terraformls.setup({})
+lspconfig.ruby_lsp.setup({})
+lspconfig.rubocop.setup({})
+lspconfig.gleam.setup({})
+lspconfig.yamlls.setup({
+  settings = {
+    yaml = {
+      format = {
+        enable = true,
+      },
+    },
+  },
+})
+lspconfig.emmet_ls.setup({
+  filetypes = { 'css', 'html', 'javascript', 'heex', 'htmldjango' },
+})
+-- lspconfig.elixirls.setup({
+--   cmd = { os.getenv('ELIXIRLS_CMD') },
+-- })
+-- lspconfig.nextls.setup({
+--   cmd = { os.getenv('NEXTLS_CMD'), '--stdio' },
+-- })
+lspconfig.rust_analyzer.setup({
+  settings = {
+    ['rust-analyzer'] = {
+      check = {
+        command = 'clippy',
+      },
+    },
+  },
+})
+
+-- Setup html language server
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+require('lspconfig').html.setup({
+  capabilities = capabilities,
+  filetypes = { 'html', 'heex', 'htmldjango', 'templ' },
+})
