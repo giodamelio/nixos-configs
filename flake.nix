50714ac4d190a822575c2f5576ace9e95abe6c8b
Giovanni d'Amelio <gio@damelio.net>
2025-01-12 13:42:17 -0800

    Create a new flake with snowfall lib

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "My personal Nix configs";
+
+  outputs = inputs: inputs.snowfall-lib.mkFlake {
+    inherit inputs;
+
+    src = ./.; # Root of the flake
+
+    snowfall = {
+      root = ./nix;
+      namespace = "giodamelio";
+    };
+  };
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # Unified config loader
+    snowfall-lib.url = "github:snowfallorg/lib";
+    snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
+  };
+}
