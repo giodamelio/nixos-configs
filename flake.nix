c5a5c23fd0d73cb115c3efd6aff1e899ffc4e170
Giovanni d'Amelio <gio@damelio.net>
2025-02-06 14:45:45 -0800

    Remove flake.parts and all it's complications. Start a basic devshell

--- flake.nix
+++ flake.nix
@@ -1,141 +1,23 @@
 {
   description = "My Personal Nix Configs";
 
-  outputs = inputs @ {self, flake-parts, ...}: let
-    inherit (inputs.nixpkgs.lib) debug;
-
-    # Static data about our homelab
-    homelab = builtins.fromTOML (builtins.readFile ./homelab.toml);
-
-    # Load all of our source file
-    lib = inputs.haumea.lib.load {
-      src = ./src;
-      inputs = {
-        inherit inputs homelab debug;
-        inherit (inputs.nixpkgs) lib;
-      };
-    };
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.treefmt-nix.flakeModule
-        inputs.pkgs-by-name-for-flake-parts.flakeModule
-        inputs.ez-configs.flakeModule
-      ];
-
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-
-      # Load configs from nix directory
-      ezConfigs = {
-        root = ./nix;
-        globalArgs = {
-          inherit inputs self homelab;
-          myPkgs = self.packages;
-        };
-
-        nixos.hosts.test-vm.userHomeModules = ["server"];
-        nixos.hosts.cadmium.userHomeModules = ["giodamelio"];
-        nixos.hosts.manganese.userHomeModules = ["server"];
-      };
-
-      perSystem = {
-        pkgs,
-        inputs',
-        config,
-        system,
-        ...
-      }: let
-        sys = {inherit pkgs inputs' config system;};
-      in {
-        # Allow unfree packages
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        # Load packages from directories
-        pkgsDirectory = ./src/pkgs-by-name;
-
-        # Create basic devshell with some basic tools
-        devShells.default = pkgs.mkShell {
-          buildInputs =
-            [
-              inputs'.colmena.packages.colmena
-              inputs'.ragenix.packages.default
-
-              config.packages.deploy
-              config.packages.neovim
-              config.packages.agedit
-
-              pkgs.git
-              pkgs.nurl
-              pkgs.nix-init
-              pkgs.nushell
-              pkgs.rage
-              pkgs.pwgen
-              pkgs.dogdns
-              pkgs.opentofu
-              pkgs.little_boxes
-              pkgs.nil
-              pkgs.nvd
-              pkgs.nix-diff
-            ]
-            ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-              inputs'.morlana.packages.default
-            ];
-        };
-
-        treefmt = {
-          projectRootFile = ".git/config";
-          programs = {
-            alejandra.enable = true;
-            stylua.enable = true;
-          };
-        };
-      };
-
-      flake = {
-        # Export our modules and configurations
-        inherit (lib) nixosModules;
-        inherit (lib) nixosConfigurations;
-        inherit (lib) darwinConfigurations;
-        inherit (lib) homeModules;
-
-        # Deploy with Colmena
-        colmenaHive = inputs.colmena.lib.makeHive self.outputs.colmena;
-        colmena =
-          {
-            meta = {
-              description = "My personal boxes";
-
-              # This can be overriden by node nixpkgs
-              nixpkgs = import inputs.nixpkgs {system = "x86_64-linux";};
-              nodeNixpkgs = builtins.mapAttrs (_: value: value.pkgs) lib.nixosConfigurations;
-              nodeSpecialArgs = builtins.mapAttrs (_: value: value._module.specialArgs) lib.nixosConfigurations;
-            };
-          }
-          // builtins.mapAttrs (_: value: {imports = value._module.args.modules;}) lib.nixosConfigurations;
-      };
-    };
+  outputs = inputs: inputs.blueprint {
+    inherit inputs;
+    prefix = ./nix;
+    nixpkgs.config.allowUnfree = true;
+  };
 
   inputs = {
     # Nixpkgs
     nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
 
+    # Make the organization of the flake easy
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+
     # Configure MacOS via Nix
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Flake authoring framework
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
-    # Easy package definition
-    pkgs-by-name-for-flake-parts.url = "github:drupol/pkgs-by-name-for-flake-parts";
-
-    # Easy config organization
-    ez-configs.url = "github:ehllie/ez-configs";
-    ez-configs.inputs.nixpkgs.follows = "nixpkgs";
-    ez-configs.inputs.flake-parts.follows = "flake-parts";
 
     # Format all the things
     treefmt-nix.url = "github:numtide/treefmt-nix";
@@ -157,10 +39,6 @@
     # Use NixOS in WSL2
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Filesystem based importing for Nix
-    haumea.url = "github:nix-community/haumea/v0.2.2";
-    haumea.inputs.nixpkgs.follows = "nixpkgs";
 
     # Manage user environments with Nix
     home-manager.url = "github:nix-community/home-manager";
