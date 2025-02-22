4862f1fdd522aaf1b2dcc84f384edce66c2573ee
Giovanni d'Amelio <gio@damelio.net>
2025-02-21 16:16:14 -0800

    Move Gatus config into a seperate module

--- nix/hosts/manganese/configuration.nix
+++ nix/hosts/manganese/configuration.nix
@@ -14,6 +14,7 @@ in {
     ./disko.nix
     ./hardware.nix
     ./prometheus.nix
+    ./gatus.nix
 
     flake.nixosModules.basic-packages
     flake.nixosModules.basic-settings
@@ -51,71 +52,6 @@ in {
         enable = true;
       };
     }
-
-    # Simple Status Page
-    (_: {
-      services.gatus = {
-        enable = true;
-        openFirewall = true;
-        settings = {
-          metrics = true;
-          storage = {
-            type = "sqlite";
-            path = "/var/lib/gatus/data.db";
-          };
-          endpoints = let
-            mkPingEndpoint = name: host: {
-              inherit name;
-              group = "Hosts";
-              url = "icmp://${host}";
-              interval = "5m";
-              conditions = [
-                "[CONNECTED] == true"
-              ];
-            };
-          in [
-            {
-              name = "Headscale";
-              url = "https://headscale.gio.ninja/health";
-              interval = "5m";
-              conditions = [
-                "[STATUS] == 200"
-                "[BODY].status == pass"
-                "[RESPONSE_TIME] < 300"
-              ];
-            }
-            {
-              name = "Prometheus";
-              url = "http://manganese.h.gio.ninja:9090/-/healthy";
-              interval = "5m";
-              conditions = [
-                "[STATUS] == 200"
-                "[RESPONSE_TIME] < 300"
-              ];
-            }
-            {
-              name = "Google";
-              url = "https://google.com";
-              interval = "10m";
-              conditions = [
-                "[STATUS] == 200"
-              ];
-            }
-            (mkPingEndpoint "cadmium" "cadmium.h.gio.ninja")
-            (mkPingEndpoint "lithium1" "lithium1.h.gio.ninja")
-            (mkPingEndpoint "manganese" "manganese.h.gio.ninja")
-          ];
-        };
-      };
-
-      # Allow Gatus to send ICMP traffic
-      systemd.services.gatus = {
-        serviceConfig = {
-          CapabilityBoundingSet = "CAP_NET_RAW";
-          AmbientCapabilities = "CAP_NET_RAW";
-        };
-      };
-    })
   ];
 
   # ZFS snapshot browsing
