abcfd05051caccf9f3723df7a186a3cacabddaaa
Giovanni d'Amelio <gio@damelio.net>
2025-04-14 16:32:27 -0700

    Move cadmium to use systemd-boot instead of grub

--- nix/hosts/cadmium/bootloader.nix
+++ nix/hosts/cadmium/bootloader.nix
@@ -1,10 +1,14 @@
-{lib, ...}: {
+_: {
   boot.loader = {
-    grub = {
-      enable = lib.mkDefault true;
-      efiSupport = true;
-      device = "nodev";
-      useOSProber = true;
+    systemd-boot = {
+      enable = true;
+      netbootxyz.enable = true;
+      windows = {
+        "11-Pro" = {
+          efiDeviceHandle = "HD0b65535a2";
+          title = "Windows 11 Pro";
+        };
+      };
     };
 
     efi.canTouchEfiVariables = true;
