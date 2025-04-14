bbbbea46c0129ab6641f19130aa4c123a54f61d8
Giovanni d'Amelio <gio@damelio.net>
2025-04-14 16:47:01 -0700

    Put Windows and netboot.xyz above NixOS in the boot order
    NixOS as the default

--- nix/hosts/cadmium/bootloader.nix
+++ nix/hosts/cadmium/bootloader.nix
@@ -2,11 +2,15 @@ _: {
   boot.loader = {
     systemd-boot = {
       enable = true;
-      netbootxyz.enable = true;
+      netbootxyz = {
+        enable = true;
+        sortKey = "m_netbootxyz";
+      };
       windows = {
         "11-Pro" = {
           efiDeviceHandle = "HD0b65535a2";
           title = "Windows 11 Pro";
+          sortKey = "m_windows";
         };
       };
     };
