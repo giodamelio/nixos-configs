65b46165aa11077f69b81e5abffa4d874f5d853c
Giovanni d'Amelio <gio@damelio.net>
2025-04-14 17:55:37 -0700

    Update deps hash

--- nix/packages/impostare.nix
+++ nix/packages/impostare.nix
@@ -10,7 +10,7 @@ pkgs.rustPlatform.buildRustPackage rec {
     hash = "sha256-+b7dBz7LQ5djXtUj/0/lu49FBK1iy7YCRVEawnkJMrE=";
   };
 
-  cargoHash = "sha256-b96NNGhQbqcUkMp2NkI7pglSP3RArcFnbnX+KoxPDvA=";
+  cargoHash = "sha256-HOQJ5Z3WJIcnoOCR8bjBoa9kWNCb1HG4bPZdnAr9/1c=";
 
   buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
     pkgs.darwin.apple_sdk.frameworks.CoreFoundation
