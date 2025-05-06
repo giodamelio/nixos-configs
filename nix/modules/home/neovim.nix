0b05b17e9778f89570b14fe2fd67299736cec7d6
Giovanni d'Amelio <gio@damelio.net>
2025-05-05 22:10:15 -0700

    Setup Neovim via Home Manager

--- nix/modules/home/neovim.nix
+++ nix/modules/home/neovim.nix
@@ -13,4 +13,9 @@ in {
   # Set Neovim as the default editor manually.
   # Since we have a custom Neovim package, we can't use the HomeManager module directly
   home.sessionVariables = {EDITOR = "${customNeovim}/bin/nvim";};
+
+  programs.neovim = {
+    package = customNeovim;
+    vimAlias = true;
+  };
 }
