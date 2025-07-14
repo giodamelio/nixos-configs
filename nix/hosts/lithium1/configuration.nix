814a81e00a901b89128ed0af65c5174937ba9646
Giovanni d'Amelio <gio@damelio.net>
2025-07-13 22:19:10 -0700

    Setup PocketID and headscale

--- nix/hosts/lithium1/configuration.nix
+++ nix/hosts/lithium1/configuration.nix
@@ -22,27 +22,86 @@ in {
       programs.zsh.enable = true;
     })
 
+    # Setup Pocket ID
+    ({pkgs, ...}: {
+      services.pocket-id = {
+        enable = true;
+
+        settings = {
+          APP_URL = "https://login.gio.ninja";
+          TRUST_PROXY = true;
+          ENCRYPTION_KEY_FILE = "\${CREDENTIALS_DIRECTORY}/pocket-id-encryption-key";
+          ANALYTICS_DISABLED = true;
+        };
+      };
+
+      # Load the encrypted encryption key
+      systemd.services.pocket-id.serviceConfig = {
+        LoadCredentialEncrypted = "pocket-id-encryption-key:/var/lib/credstore/pocket-id-encryption-key";
+      };
+
+      # Setup Caddy as a reverse proxy
+      networking.firewall.allowedTCPPorts = [80 443];
+      services.caddy = {
+        enable = true;
+
+        virtualHosts."https://login.gio.ninja" = {
+          extraConfig = ''
+            reverse_proxy localhost:1411
+          '';
+        };
+
+        virtualHosts."https://headscale.gio.ninja" = {
+          extraConfig = ''
+            reverse_proxy localhost:8080
+          '';
+        };
+      };
+    })
+
     # Run Headscale for easy networking
     {
       networking.firewall.allowedTCPPorts = [80 443];
 
       services.headscale = {
         enable = true;
-        address = "0.0.0.0";
-        port = 443;
+        port = 8080;
 
         settings = {
-          server_url = "https://headscale.gio.ninja:443";
+          server_url = "https://headscale.gio.ninja";
 
-          tls_letsencrypt_hostname = "headscale.gio.ninja";
+          tls_cert_path = null;
+          tls_key_paht = null;
 
           dns = {
             magic_dns = true;
             base_domain = "h.gio.ninja";
+            nameservers.global = ["1.1.1.1" "1.0.0.1"];
+          };
+
+          oidc = {
+            only_start_if_oidc_is_available = true;
+            issuer = "https://login.gio.ninja";
+            client_id = "251934f5-6b41-4665-9a7f-c475ca534c92";
+            client_secret_path = "\${CREDENTIALS_DIRECTORY}/headscale-oidc-client-secret";
+            scope = ["openid" "profile" "email" "groups"];
+            allowed_groups = ["headscale"];
+            pkce = {
+              enabled = true;
+              method = "S256";
+            };
           };
         };
       };
 
+      # Load the encrypted encryption key
+      systemd.services.headscale.serviceConfig = {
+        LoadCredentialEncrypted = "headscale-oidc-client-secret:/var/lib/credstore/headscale-oidc-client-secret";
+      };
+    }
+
+    # Enable Tailscale
+    {
       services.tailscale = {
         enable = true;
       };
