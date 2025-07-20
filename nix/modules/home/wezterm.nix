1db6531a50b72773c0aefe396b7c0d85f052e4d0
Giovanni d'Amelio <gio@damelio.net>
2025-07-20 13:30:26 -0700

    A bunch of random Wezterm updates

--- nix/modules/home/wezterm.nix
+++ nix/modules/home/wezterm.nix
@@ -5,46 +5,99 @@ _: {
 
     extraConfig = ''
       local action = wezterm.action
+      local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
-      return {
-        font = wezterm.font("JetBrainsMono Nerd Font"),
-        font_size = 12.0,
-        harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, -- Disable ligatures
+      local config = wezterm.config_builder()
 
-        hide_tab_bar_if_only_one_tab = true,
+      -- Color Scheme
+      config.color_scheme = 'Molokai'
 
-        -- TODO: Update this when there is highlighting support
-        -- See: https://github.com/wezterm/wezterm/issues/4077
-        hyperlink_rules = wezterm.default_hyperlink_rules(),
+      -- Font Setup
+      config.font = wezterm.font("JetBrainsMono Nerd Font")
+      config.font_size = 12.0
+      config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable ligature
+
+      config.hide_tab_bar_if_only_one_tab = false
+      config.use_fancy_tab_bar = true
+
+      -- TODO: Update this when there is highlighting support
+      -- See: https://github.com/wezterm/wezterm/issues/4077
+      config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
         -- Minimize padding
-        window_padding = {
-          left = 0,
-          right = 0,
-          top = 0,
-          bottom = 0,
-        },
-
-        -- Show scroll bar
-        enable_scroll_bar = true,
-
-        -- Keep more scrollback lines
-        scrollback_lines = 6000;
-
-        -- Make Mouse scrolling smaller then a page
-        mouse_bindings = {
-          {
-            event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-            mods = 'NONE',
-            action = action.ScrollByLine(-3),
-          },
-          {
-            event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-            mods = 'NONE',
-            action = action.ScrollByLine(3),
-          },
-        }
+      config.window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
       }
+
+      -- Show scroll bar
+      config.enable_scroll_bar = true
+
+      -- Keep more scrollback lines
+      config.scrollback_lines = 6000
+
+      -- Key Bindings
+      config.keys = {
+        { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+      }
+
+      -- Make Mouse scrolling smaller then a page
+      config.mouse_bindings = {
+        {
+          event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+          mods = 'NONE',
+          action = action.ScrollByLine(-3),
+        },
+        {
+          event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+          mods = 'NONE',
+          action = action.ScrollByLine(3),
+        },
+      }
+
+      -- bar.apply_to_config(config)
+      tabline.setup({
+        options = {
+          icons_enabled = true,
+          tabs_enabled = true,
+          theme_overrides = {},
+          section_separators = {
+            left = wezterm.nerdfonts.pl_left_hard_divider,
+            right = wezterm.nerdfonts.pl_right_hard_divider,
+          },
+          component_separators = {
+            left = wezterm.nerdfonts.pl_left_soft_divider,
+            right = wezterm.nerdfonts.pl_right_soft_divider,
+          },
+          tab_separators = {
+            left = ''\'',
+            right = ''\'',
+          },
+        },
+        sections = {
+          tabline_a = { 'workspace' },
+          tabline_b = { 'window' },
+          tabline_c = { },
+          tab_active = {
+            'index',
+            { 'parent', padding = 0 },
+            '/',
+            { 'cwd', padding = { left = 0, right = 1 } },
+            { 'zoomed', padding = 0 },
+          },
+          tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+          tabline_x = { },
+          tabline_y = { },
+          tabline_z = { 'hostname' },
+        },
+        extensions = {},
+      })
+
+      tabline.apply_to_config(config)
+
+      return config
     '';
   };
 }
