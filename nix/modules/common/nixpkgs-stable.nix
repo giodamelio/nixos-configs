44d70b9143bfbc7ac27b8fe2a32c573e87365344
Giovanni d'Amelio <gio@damelio.net>
2025-07-20 21:58:52 -0700

    Use Nixpkgs stable version of Datagrip

---
+++ nix/modules/common/nixpkgs-stable.nix
@@ -0,0 +1,6 @@
+{ inputs, pkgs, ... }: {
+  _module.args.pkgsStable = import inputs.nixpkgs-stable {
+    inherit (pkgs) system;
+    config.allowUnfree = true;
+  };
+}
--- nix/modules/nixos/software-development.nix
+++ nix/modules/nixos/software-development.nix
@@ -1,7 +1,11 @@
-{pkgs, ...}: {
+{flake, pkgs, pkgsStable, ...}: {
+  imports = [
+    flake.modules.common.nixpkgs-stable
+  ];
+
   environment.systemPackages = with pkgs; [
     devenv # Development environment manager
-    jetbrains.datagrip # Database IDE from JetBrains
+    pkgsStable.jetbrains.datagrip # Database IDE from JetBrains
     spotify # Music streaming client
     aider-chat # AI pair programming assistant using OpenAI
     code-cursor # AI code editing assistant using cursor.so
