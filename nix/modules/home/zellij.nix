77e4c895d1e0b4ab6dc15f4a307e8c47fb3a1bb3
Giovanni d'Amelio <gio@damelio.net>
2025-08-14 15:23:48 -0700

    Rollback confusing Zellij bindings

--- nix/modules/home/zellij.nix
+++ nix/modules/home/zellij.nix
@@ -15,23 +15,6 @@
 
   # Manually set KDL config string, since the HM module is not in great shape right now
   xdg.configFile."zellij/config.kdl".text = ''
-    keybinds {
-      // Replace quit with detach
-      shared_except "locked" {
-        bind "Ctrl q" { Detach; }
-      }
-
-      // Override so double Ctrl-o go straight to the session switcher
-      shared_except "session" "locked" {
-        bind "Ctrl o" {
-          SwitchToMode "Session";
-          LaunchOrFocusPlugin "session-manager" {
-              floating true
-              move_to_focused_tab true
-          };
-          SwitchToMode "Normal";
-        }
-      }
-    }
+    keybinds {}
   '';
 }
