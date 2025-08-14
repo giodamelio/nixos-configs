fd08fbac7eb6f02757ba76b97640ce6fae446d3e
Giovanni d'Amelio <gio.damelio@logixboard.com>
2025-08-14 16:14:02 -0700

    Wezterm was getting too fancy

--- nix/modules/home/wezterm.nix
+++ nix/modules/home/wezterm.nix
@@ -6,12 +6,9 @@
 
     extraConfig = ''
       local action = wezterm.action
-      local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
       local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
 
       local config = wezterm.config_builder()
-
-      -- config.window_decorations = 'NONE'
 
       -- Color Scheme
       config.color_scheme = 'Molokai'
@@ -22,7 +19,7 @@
       config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable ligature
 
       config.hide_tab_bar_if_only_one_tab = false
-      config.use_fancy_tab_bar = true
+      config.use_fancy_tab_bar = false
 
       -- TODO: Update this when there is highlighting support
       -- See: https://github.com/wezterm/wezterm/issues/4077
@@ -60,46 +57,6 @@
           action = action.ScrollByLine(3),
         },
       }
-
-      -- bar.apply_to_config(config)
-      tabline.setup({
-        options = {
-          icons_enabled = true,
-          tabs_enabled = true,
-          theme_overrides = {},
-          section_separators = {
-            left = wezterm.nerdfonts.pl_left_hard_divider,
-            right = wezterm.nerdfonts.pl_right_hard_divider,
-          },
-          component_separators = {
-            left = wezterm.nerdfonts.pl_left_soft_divider,
-            right = wezterm.nerdfonts.pl_right_soft_divider,
-          },
-          tab_separators = {
-            left = ''\'',
-            right = ''\'',
-          },
-        },
-        sections = {
-          tabline_a = { 'workspace' },
-          tabline_b = { 'window' },
-          tabline_c = { },
-          tab_active = {
-            'index',
-            { 'parent', padding = 0 },
-            '/',
-            { 'cwd', padding = { left = 0, right = 1 } },
-            { 'zoomed', padding = 0 },
-          },
-          tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
-          tabline_x = { },
-          tabline_y = { },
-          tabline_z = { 'hostname' },
-        },
-        extensions = {},
-      })
-
-      -- tabline.apply_to_config(config)
 
       smart_splits.apply_to_config(config, {
         direction_keys = { 'h', 'j', 'k', 'l' },
