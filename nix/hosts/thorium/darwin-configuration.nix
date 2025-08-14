4a156c807f52b18cc776224c6f9f18106f9a91d4
Giovanni d'Amelio <gio.damelio@logixboard.com>
2025-08-14 15:38:15 -0700

    Misc MacOS and Zsh updates

--- nix/hosts/thorium/darwin-configuration.nix
+++ nix/hosts/thorium/darwin-configuration.nix
@@ -1,4 +1,4 @@
-{flake, ...}: {
+{ config, flake, pkgs, ... }: {
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
@@ -7,13 +7,28 @@
 
   # Setup the users home
   users.users.giodamelio.home = /Users/giodamelio;
+  system.primaryUser = "giodamelio";
 
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
-  # environment.systemPackages = [
-  #   flake.packages.neovim
-  # ];
+  # Install some random packages that we need
+  environment.systemPackages = with pkgs; [
+    gnused
+    python312Packages.pgsanity
+    postgresql_15
+    fblog
+    logdy
+  ];
+
+  # Setup homebrew
+  homebrew = {
+    enable = true;
+  };
+
+  # Allow keyboard keys to repeat when held down
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 10;
+  system.defaults.NSGlobalDomain.KeyRepeat = 1; 
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
--- nix/modules/home/zsh.nix
+++ nix/modules/home/zsh.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{flake, pkgs, lib, config, ...}: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -17,16 +13,33 @@
     ];
 
     initContent = let
-      # Setup Homebrew path on MacOS
-      homebrewShellEnv = lib.mkAfter ''
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      '';
-
       # Fix Vim Mode keybindings issue
       # See: https://github.com/jeffreytse/zsh-vi-mode/issues/148#issuecomment-1566863380
       vimModeKeybindingFix = lib.mkAfter ''
         zvm_after_init_commands+=("bindkey -M viins '^r' atuin-search-viins")
       '';
+
+      # Add some things to the path
+      homebrewShellEnv = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin (let
+        brewBinPath = flake.outputs.darwinConfigurations.thorium.config.homebrew.brewPrefix;
+        # The one from nix-darwin has the /bin attached, remove that
+        brewPrefix = builtins.dirOf brewBinPath;
+      in lib.mkAfter ''
+        # Setup Homebrew path on MacOS
+        # Set the homebrew PATH after the NIX path so Nix executables take precedent
+        # Not using `brew shellenv` since it always puts itself first in the $PATH
+        # Note: Hostname has to be hardcoded for now
+        # See: https://github.com/numtide/blueprint/issues/69
+        export HOMEBREW_PREFIX="${brewPrefix}";
+        export HOMEBREW_CELLAR="${brewPrefix}/Cellar";
+        export HOMEBREW_REPOSITORY="${brewPrefix}";
+        export PATH="$PATH:${brewPrefix}/bin";
+        export INFOPATH="${brewPrefix}/share/info:${"\${INFOPATH:-}"}";
+
+        # Add ~/.local/bin to the start of the PATH
+        # ''\ is a weird escape sequence
+        export PATH="''\${HOME}/.local/bin:$PATH"
+      '');
     in
       lib.mkMerge (
         [vimModeKeybindingFix]
