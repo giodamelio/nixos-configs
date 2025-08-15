610b0bc9fae7af3aa7bcac2d800ae31df0298fd4
Giovanni d'Amelio <gio.damelio@logixboard.com>
2025-08-14 20:16:30 -0700

    Add an improved version of the tree alias for zsh
    Pagers are amazing

--- nix/modules/home/modern-coreutils-replacements.nix
+++ nix/modules/home/modern-coreutils-replacements.nix
@@ -1,4 +1,6 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  inherit (pkgs) lib;
+in {
   home.packages = with pkgs; [
     dust
     procs
@@ -9,6 +11,17 @@
     du = "dust";
     ps = pkgs.lib.getExe pkgs.procs;
   };
+
+  # Improved version of eza tree for zsh that uses a pager
+  # Aliases are at 1100, so going after that overrides them
+  # Args: -R color -F exits if content is less then one page -i case insensitive search
+  programs.zsh.initContent = lib.mkOrder 1200 ''
+    # Override the alias with a better version that uses a pager
+    unalias tree
+    tree() {
+      eza --tree --color=always $@ | less -RFi
+    }
+  '';
 
   programs = {
     # ls
