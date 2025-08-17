7970e56b198e63c4c59ee0a549e72a384acc3d63
Giovanni d'Amelio <gio@damelio.net>
2025-08-16 17:24:25 -0700

    Use nh as my remote deployer

--- deploy.sh
+++ deploy.sh
@@ -15,21 +15,6 @@ fi
 HOSTNAME=$1
 SSH_DESTINATION=$2
 
-# Build the system configuration
-nom build .#nixosConfigurations."$HOSTNAME".config.system.build.toplevel
-
-# Extract the store path from the result
-STORE_PATH=$(readlink -f ./result)
-
-# Copy the built system to the target machine
-nix copy --substitute-on-destination --to ssh://"$SSH_DESTINATION" "$STORE_PATH"
-
-# Switch to the new config
-ssh "$SSH_DESTINATION" "sudo $STORE_PATH/bin/switch-to-configuration switch"
-
-# Set the nix profile to the new system
-ssh "$SSH_DESTINATION" "sudo nix-env --profile /nix/var/nix/profiles/system --set $STORE_PATH"
-
-# I need this to get a new systemd-boot entry, I didn't think I would need this though
-ssh "$SSH_DESTINATION" "sudo /run/current-system/bin/switch-to-configuration boot"
+# Use NH to build and deploy
+nh os switch --hostname "$HOSTNAME" --target-host "$SSH_DESTINATION" .
 
