265ed7136a2b117879ff5a4b32d2154cb77b5f44
Giovanni d'Amelio <gio.damelio@logixboard.com>
2025-08-19 15:23:44 -0700

    Fix it so this command builds a dummy command on Darwin

--- nix/packages/audio-output-switcher.nix
+++ nix/packages/audio-output-switcher.nix
@@ -2,30 +2,42 @@
   flake,
   pkgs,
   ...
-}:
-flake.lib.writeNushellApplication pkgs {
-  name = "audio-output-switcher";
-  runtimeInputs = [pkgs.pulseaudio pkgs.wofi];
-  source = ''
-    let sinks = (pactl --format=json list sinks | from json)
+}: let
+  inherit (pkgs) lib;
 
-    let sink_map = ($sinks | each {|x|
-      {
-        name: $x.name
-        desc: ($x.description | str trim)
+  cmd = flake.lib.writeNushellApplication pkgs {
+    name = "audio-output-switcher";
+    runtimeInputs = [pkgs.pulseaudio pkgs.wofi];
+    source = ''
+      let sinks = (pactl --format=json list sinks | from json)
+
+      let sink_map = ($sinks | each {|x|
+        {
+          name: $x.name
+          desc: ($x.description | str trim)
+        }
+      } | filter {|x| $x.desc != null })
+
+      let choices = ($sink_map | get desc | str join "\n")
+      let selected_desc = (echo $choices | wofi --dmenu --prompt "Select Audio Output" | str trim)
+
+      let selected = ($sink_map | where desc == ($selected_desc | str trim))
+      if ($selected | is-empty) == false {
+        let selected_name = ($selected | get 0 | get name)
+        pactl set-default-sink $selected_name
+        for stream in (pactl list short sink-inputs | lines | each { |l| $l | split row "\t" | get 0 }) {
+          pactl move-sink-input $stream $selected_name
+        }
       }
-    } | filter {|x| $x.desc != null })
+    '';
+  };
 
-    let choices = ($sink_map | get desc | str join "\n")
-    let selected_desc = (echo $choices | wofi --dmenu --prompt "Select Audio Output" | str trim)
-
-    let selected = ($sink_map | where desc == ($selected_desc | str trim))
-    if ($selected | is-empty) == false {
-      let selected_name = ($selected | get 0 | get name)
-      pactl set-default-sink $selected_name
-      for stream in (pactl list short sink-inputs | lines | each { |l| $l | split row "\t" | get 0 }) {
-        pactl move-sink-input $stream $selected_name
-      }
-    }
+  dummy = pkgs.writeShellScriptBin "audio-output-switcher" ''
+    echo "This application is only available on Linux" >&2
+    echo "You are running: $(uname -s)" >&2
+    exit 1
   '';
-}
+in
+  if pkgs.stdenv.isLinux
+  then cmd
+  else dummy
