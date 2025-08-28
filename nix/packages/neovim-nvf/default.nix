db2c1a6a0034610a1dba83b5091d8d67e999ca05
Giovanni d'Amelio <gio.damelio@logixboard.com>
2025-08-28 14:05:48 -0700

    Copy over basic vim options

--- nix/packages/neovim-nvf/default.nix
+++ nix/packages/neovim-nvf/default.nix
@@ -20,6 +20,49 @@ in
 
       {
         config.vim = {
+          # Use space as the leader key and , as the localleader
+          globals.mapleader = " ";
+          globals.maplocalleader = ",";
+
+          options = {
+            # Set the default tab to 2 spaces
+            tabstop = 2;
+            shiftwidth = 2;
+            softtabstop = 2;
+            expandtab = true; # Use spaces instead of tabs
+
+            # Show relative line numbers
+            number = true;
+            relativenumber = true;
+
+            # Make search smarter
+            ignorecase = true; # Case insensitive search
+            smartcase = true; # If there are uppercase letters, become case-sensitive
+
+            # Use the system clipboard by default
+            clipboard = "unnamedplus";
+
+            # Show trailing spaces as dots
+            list = true;
+            listchars = "trail:·,tab:  ";
+
+            # Highlight the line the cursor is on
+            cursorline = true;
+
+            # Completely disable the mouse
+            mouse = "";
+
+            # Options for completions
+            completeopt = "menu,menuone,noselect";
+
+            # Enable 24 bit colors requires ISO-8613-3 compatible terminal
+            termguicolors = true;
+
+            # File change detection - reload files when they change on disk
+            autoread = true; # Automatically read file when changed outside vim
+            updatetime = 100; # Faster update time (default is 4000ms)
+          };
+
           keybindingTree = {
             groups = {
               "<leader>" = {
