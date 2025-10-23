957dce0ca9c4a12d00c49a09a33b75e357a885ac
Giovanni d'Amelio <gio@damelio.net>
2025-10-23 16:34:34 -0700

    Add 1Password GUI to basic programs list

--- nix/modules/nixos/basic-packages-desktop.nix
+++ nix/modules/nixos/basic-packages-desktop.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
   environment = {
     systemPackages = with pkgs; [
-      bitwarden-cli
-      bitwarden-menu
       thunderbird
       obsidian
       pavucontrol
@@ -17,5 +15,13 @@
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
     };
+  };
+
+  programs._1password-gui = {
+    enable = true;
+
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = ["giodamelio"];
   };
 }
