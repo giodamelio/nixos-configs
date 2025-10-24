9419efbd5010b3dfb02d32a2298f040d9fb17432
Giovanni d'Amelio <gio@damelio.net>
2025-10-24 08:35:02 -0700

    Add a script to sync 1Password secrets with SystemD Creds

---
+++ nix/packages/sync-1password-secrets/default.nix
@@ -0,0 +1,42 @@
+{pkgs, ...}: let
+  inherit (pkgs) lib;
+
+  onepassword-sdk-python = pkgs.python3.pkgs.buildPythonPackage rec {
+    pname = "onepassword-sdk-python";
+    version = "0.3.1";
+    pyproject = true;
+
+    src = pkgs.fetchFromGitHub {
+      owner = "1Password";
+      repo = "onepassword-sdk-python";
+      rev = "v${version}";
+      hash = "sha256-nPJEtBztF+e+gcgY3+4u6CK6h9QDzqw1OhAqX0PaaAQ=";
+    };
+
+    build-system = [
+      pkgs.python3.pkgs.setuptools
+      pkgs.python3.pkgs.wheel
+    ];
+
+    propagatedBuildInputs = with pkgs.python3.pkgs; [
+      pydantic
+    ];
+
+    pythonImportsCheck = [
+      "onepassword"
+    ];
+
+    meta = {
+      description = "";
+      homepage = "https://github.com/1Password/onepassword-sdk-python";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [giodamelio];
+    };
+  };
+in
+  pkgs.writers.writePython3Bin "sync-secrets.py" {
+    libraries = with pkgs.python3.pkgs; [
+      pwinput
+      onepassword-sdk-python
+    ];
+  } (builtins.readFile ./sync-1password-secrets.py)
---
+++ nix/packages/sync-1password-secrets/sync-1password-secrets.py
@@ -0,0 +1,91 @@
+import os
+import asyncio
+import subprocess
+import platform
+
+import pwinput
+from onepassword.client import Client
+
+
+def write_systemd_secret(name, contents):
+    try:
+        proc = subprocess.Popen(
+            [
+                "systemd-creds",
+                "encrypt",
+                "--name=" + name,
+                "-",
+                f"/var/lib/credstore/{name}"
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        stdout, stderr = proc.communicate(input=contents)
+
+        if proc.returncode == 0:
+            print("Credential Synced")
+        else:
+            print(f"Error encrypting credential: {stderr.strip()}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def is_password_field(item):
+    return item.title == "password"
+
+
+async def main():
+    if os.geteuid() != 0:
+        exit("Script must have root permissions")
+
+    prompt = "Enter 1Password Service Account Token: "
+    token = pwinput.pwinput(prompt=prompt).strip()
+
+    client = await Client.authenticate(
+      auth=token,
+      integration_name="1Password systemd-creds sync",
+      integration_version="v1.0.0"
+    )
+
+    # Find the correct vault
+    vaults = await client.vaults.list()
+    vault = next(filter(lambda v: v.title == "Homelab", vaults))
+
+    # Get the hostname
+    hostname = platform.node()
+    hostname_tag = f"host-{hostname}"
+
+    # Get all the items
+    overviews = await client.items.list(vault.id)
+    for overview in overviews:
+        item = await client.items.get(vault.id, overview.id)
+        print(f"Fetching {item.title}")
+
+        # Only sync items that have the proper tag for this host or the all tag
+        if hostname_tag not in item.tags and "host-all" not in item.tags:
+            print("Syncing Skipped")
+            print()
+            continue
+
+        # If it's a password just write the value out to a file
+        # If it's a secure note construct a envfile from all the fields
+        if item.category == "Password":
+            field = next(filter(is_password_field, item.fields), None)
+            write_systemd_secret(item.title, field.value)
+        elif item.category == "SecureNote":
+            # Build the env from all the fields
+            envfile = ""
+            for field in item.fields:
+                if field.field_type == "Concealed":
+                    envfile += f"{field.title}={field.value}"
+            envfile += "\n"
+
+            write_systemd_secret(item.title, envfile)
+
+        print()
+
+asyncio.run(main())
