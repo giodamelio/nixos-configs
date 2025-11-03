54266649fc0197fe65da042cb52cb2b230c23ad4
Giovanni d'Amelio <gio@damelio.net>
2025-11-03 09:34:39 -0800

    Allow opening Wezterm scrollback in /nix/store/mf316jkp3dhypxi57viynyxfijwc7a59-nvim/bin/nvim

--- nix/modules/home/wezterm.nix
+++ nix/modules/home/wezterm.nix
@@ -5,6 +5,10 @@
     package = perSystem.nixpkgs-stable.wezterm;
 
     extraConfig = ''
+      local io = require 'io'
+      local os = require 'os'
+
+      local wezterm = require 'wezterm'
       local action = wezterm.action
       local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
 
@@ -49,6 +53,11 @@
       -- Key Bindings
       config.keys = {
         { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+        {
+          key = 'E',
+          mods = 'CTRL',
+          action = action.EmitEvent 'trigger-vim-with-scrollback',
+        },
       }
 
       -- Make Mouse scrolling smaller then a page
@@ -72,6 +81,36 @@
           resize = 'META',
         },
       })
+
+      -- Allow opening the whole scrollback in vim
+      wezterm.on('trigger-vim-with-scrollback', function(window, pane)
+        -- Retrieve the text from the pane
+        local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+
+        -- Create a temporary file to pass to vim
+        local name = os.tmpname()
+        local f = io.open(name, 'w+')
+        f:write(text)
+        f:flush()
+        f:close()
+
+        -- Open a new window running vim and tell it to open the file
+        window:perform_action(
+          action.SpawnCommandInNewWindow {
+            args = { 'vim', name },
+          },
+          pane
+        )
+
+        -- Wait "enough" time for vim to read the file before we remove it.
+        -- The window creation and process spawn are asynchronous wrt. running
+        -- this script and are not awaitable, so we just pick a number.
+        --
+        -- Note: We don't strictly need to remove this file, but it is nice
+        -- to avoid cluttering up the temporary directory.
+        wezterm.sleep_ms(1000)
+        os.remove(name)
+      end)
 
       return config
     '';
