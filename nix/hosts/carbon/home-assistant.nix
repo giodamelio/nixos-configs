6cbe1e72a2c4c10760df0aed59f42811fb9ab5d1
Giovanni d'Amelio <gio@damelio.net>
2026-01-03 13:33:42 -0800

    Add home assistant automation for grow light

--- nix/hosts/carbon/home-assistant.nix
+++ nix/hosts/carbon/home-assistant.nix
@@ -21,9 +21,44 @@ in {
       default_config = {};
 
       zha = {
-        usb_path = "/dev/serial/by-id/usb-Silicon_Labs_HubZ_Smart_Home_Controller_515015BA-if01-port0";
+        # usb_path = "/dev/serial/by-id/usb-Silicon_Labs_HubZ_Smart_Home_Controller_515015BA-if01-port0";
         database_path = "/var/lib/hass/zigbee.db";
       };
+
+      automation = [
+        {
+          alias = "Turn off Grow Light";
+          description = "";
+          trigger = {
+            platform = "time";
+            at = "15:00:00";
+          };
+          condition = [];
+          action = {
+            type = "turn_off";
+            device_id = "6a23d17b71a7c4437088b8e2d6c429ae";
+            entity_id = "a531393bef66e2769bb691086c8c5c35";
+            domain = "switch";
+          };
+          mode = "single";
+        }
+        {
+          alias = "Turn on Grow Light";
+          description = "";
+          trigger = {
+            platform = "time";
+            at = "10:00:00";
+          };
+          condition = [];
+          action = {
+            type = "turn_on";
+            device_id = "6a23d17b71a7c4437088b8e2d6c429ae";
+            entity_id = "a531393bef66e2769bb691086c8c5c35";
+            domain = "switch";
+          };
+          mode = "single";
+        }
+      ];
     };
     extraComponents = [
       # Z-Wave
