4226bbfb900a7fd16dd0e879cb2c5adbf77d1164
Giovanni d'Amelio <gio@damelio.net>
2026-01-07 22:59:56 -0800

    Modify reverse proxy module to allow for Unix sockets

--- nix/modules/nixos/reverse-proxy.nix
+++ nix/modules/nixos/reverse-proxy.nix
@@ -11,15 +11,24 @@
       type = types.attrsOf (types.submodule {
         options = {
           host = mkOption {
-            type = types.str;
+            type = types.nullOr types.str;
+            default = null;
             description = "The upstream host to proxy to";
             example = "localhost";
           };
 
           port = mkOption {
-            type = types.int;
+            type = types.nullOr types.int;
+            default = null;
             description = "The upstream port to proxy to";
             example = 8080;
+          };
+
+          socket_path = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = "The upstream Unix socket path to proxy to";
+            example = "/run/openbao/openbao.sock";
           };
 
           reverseProxy = mkOption {
@@ -46,12 +55,25 @@
             host = "127.0.0.1";
             port = 3000;
           };
+          "openbao" = {
+            socket_path = "/run/openbao/openbao.sock";
+          };
         }
       '';
     };
   };
 
   config = lib.mkIf config.services.gio.reverse-proxy.enable {
+    assertions = lib.flatten (
+      lib.mapAttrsToList (
+        hostname: cfg: {
+          assertion = (cfg.socket_path != null) != (cfg.host != null && cfg.port != null);
+          message = "Virtual host '${hostname}' must specify either socket_path OR both host and port";
+        }
+      )
+      config.services.gio.reverse-proxy.virtualHosts
+    );
+
     # Setup Caddy as a reverse proxy
     systemd.services.caddy.serviceConfig = {
       LoadCredentialEncrypted = [
@@ -88,9 +110,11 @@
                 ${cfg.extraConfig}
 
                 ${
-                  lib.optionalString
-                  cfg.reverseProxy
-                  "reverse_proxy ${cfg.host}:${toString cfg.port}"
+                  lib.optionalString cfg.reverseProxy (
+                    if cfg.socket_path != null
+                    then "reverse_proxy unix/${cfg.socket_path}"
+                    else "reverse_proxy ${cfg.host}:${toString cfg.port}"
+                  )
                 }
               '';
             }
