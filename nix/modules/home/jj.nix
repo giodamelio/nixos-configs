94b7bafa6cb091c67a796ffa4184ccfedb1b298d
Giovanni d'Amelio <gio@damelio.net>
2026-03-04 17:20:50 -0800

    Update starship shell for JJ

--- nix/modules/home/jj.nix
+++ nix/modules/home/jj.nix
@@ -26,4 +26,69 @@
   home.packages = with pkgs; [
     difftastic
   ];
+
+  # If we are using JJ update our shell prompt to display it
+  # This will disable the git modules when it detects a jj repo
+  # It does that by disabling the default git modules then re-invoking them with a conditional
+  programs.starship = {
+    settings = let
+      mkOverridedGit = starship_module: {
+        when = "! jj --ignore-working-copy root";
+        command = "starship module ${starship_module}";
+        description = "Only show ${starship_module} if we're not in a jj repo";
+        style = "";
+      };
+    in {
+      # First disable the built in modules
+      git_status.disabled = true;
+      git_commit.disabled = true;
+      git_metrics.disabled = true;
+      git_branch.disabled = true;
+
+      # Add back the overridden version
+      custom.git_status = mkOverridedGit "git_status";
+      custom.git_commit = mkOverridedGit "git_commit";
+      custom.git_metrics = mkOverridedGit "git_metrics";
+      custom.git_branch = mkOverridedGit "git_branch";
+
+      # Add new module to show JJ status
+      custom.jj = {
+        description = "The current jj status";
+        when = "jj --ignore-working-copy root";
+        symbol = "🥋 ";
+        command = ''
+          jj log 2>/dev/null --no-graph --ignore-working-copy --color=always --revisions @ \
+            --template '
+              surround(
+                "(",
+                ")",
+                separate(
+                  " ",
+                  bookmarks.join(", "),
+                  change_id.shortest(),
+                  commit_id.shortest(),
+                  if(conflict, label("conflict", "×")),
+                  if(divergent, label("divergent", "??")),
+                  if(hidden, label("hidden prefix", "(hidden)")),
+                  if(immutable, label("node immutable", "◆")),
+                  coalesce(
+                    if(
+                      empty,
+                      coalesce(
+                        if(
+                            parents.len() > 1,
+                            label("empty", "(merged)"),
+                        ),
+                        label("empty", "(empty)"),
+                      ),
+                    ),
+                    label("description placeholder", "*")
+                  ),
+                )
+              )
+            '
+        '';
+      };
+    };
+  };
 }
