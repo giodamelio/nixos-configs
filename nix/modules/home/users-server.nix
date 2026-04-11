ab1caee4e36e7b5c388563617029033a5e757e1b
Giovanni d'Amelio <gio@damelio.net>
2026-04-11 15:48:43 -0700

    Add shared users-server home module and refactor server configs

---
+++ nix/modules/home/users-server.nix
@@ -0,0 +1,25 @@
+{flake, ...}: {
+  imports = [
+    flake.homeModules.required
+    flake.homeModules.lil-scripts
+    flake.homeModules.git
+    flake.homeModules.neovim
+    flake.homeModules.zsh
+    flake.homeModules.atuin
+  ];
+
+  home = {
+    username = "server";
+    homeDirectory = "/home/server";
+    stateVersion = "25.05";
+  };
+
+  programs.git = {
+    enable = true;
+  };
+
+  # Configure nix-activate for NixOS
+  gio.nix-activate-config.activation = {system = "nixos";};
+
+  gio.role = "server";
+}
